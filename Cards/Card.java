447b09c575f6e85ccf2f9b097a76bade56219a82
Ayush@756 <gharaayush5@gmail.com>
2026-02-01 09:17:38 +0000

    Project started

---
+++ Cards/Card.java
@@ -0,0 +1,4 @@
+public class Card{
+    private Color color;
+    private String action;
+}
---
+++ Cards/Color.java
@@ -0,0 +1,8 @@
+public enum Color{
+    RED("Red"), BLUE("Blue"), GREEN("Green"), YELLOW("Yellow"), WILD("Wild");
+
+    private String color;
+    private Color(String color){
+        this.color = color;
+    }
+}
