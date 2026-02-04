1ad4ac74e35c2611c53a3792a7823f47b759c6c2
Hunter@756 <gharaayush5@gmail.com>
2026-02-04 23:28:03 +0530

    some code made to follow the convection

--- src/main/java/org/th/Cards/Color.java
+++ src/main/java/org/th/Cards/Color.java
@@ -4,7 +4,7 @@ public enum Color{
     RED("Red"), BLUE("Blue"), GREEN("Green"), YELLOW("Yellow"), WILD("Wild");
 
     private String color;
-    private Color(String color){
+    Color(String color){
         this.color = color;
     }
 }
