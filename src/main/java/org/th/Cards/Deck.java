608998492c1d2842858dec07c2f62378fb6718b8
Ayush@756 <gharaayush5@gmail.com>
2026-02-05 08:31:22 +0530

    Update Deck.java: Deck matches the color and action on the card

--- src/main/java/org/th/Cards/Deck.java
+++ src/main/java/org/th/Cards/Deck.java
@@ -22,18 +22,22 @@ public class Deck {
         while (true) {
             System.out.println("Your deck: ");
             cards.forEach(card -> System.out.print(card.getColor() + ":"+card.getAction()+" ");
-            System.out.println("\nEnter which card you want to play: ");
+            System.out.println("\nEnter color of card you want to play: ");
 
-            String choice = sc.next(); // Player's card input
+            String color_choice = sc.next();
+            System.out.println("\nEnter the action on the card");
+            String action_choice = sc.next(); // Player's card input
 
             List<Card> matchedCards = cards
                     .stream()
-                    .filter(c -> c.getAction().toString().equals(choice)) // filtered the matched cards out
+                    .filter(c ->
+ c.getAction().toString().equals(action_choice) &&
+c.getColor().toString().equals(color_choice)) // filtered the matched cards out
                     .toList();
 
             if (!matchedCards.isEmpty()) {
                 Card played = matchedCards.get(0); // get the first match
-                System.out.println("Card played: " + played.getAction());
+                System.out.println("Card played: " + played.getColor()+ ": " + played.getAction());
                 cards.remove(played); // remove the played card from deck
                 break; // exit loop after successful play
             } else {
