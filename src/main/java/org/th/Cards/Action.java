28af5dd2d578a6b9a016f02712876488f91845bd
Ayush@756 <gharaayush5@gmail.com>
2026-02-09 08:45:02 +0530

    Update Action.java: Added the remaining cards

--- src/main/java/org/th/Cards/Action.java
+++ src/main/java/org/th/Cards/Action.java
@@ -2,7 +2,8 @@ package org.th.Cards;
 
 public enum Action {
     ONE("1"),TWO("2"),THREE("3"),FOUR("4"),FIVE("5"),
-    SIX("6"),SEVEN("7"),EIGHT("8"),NINE("9");
+    SIX("6"),SEVEN("7"),EIGHT("8"),NINE("9"),
+REVERSE("reverse"), SKIP("skip"),ZERO("0");
 
     private final String value;
 
